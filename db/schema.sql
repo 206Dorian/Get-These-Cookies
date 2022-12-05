4a59817fc35129b020016225bf84e88aa80c09c0
Dorian Birch <206dorian@gmail.com>
2022-12-04 19:17:20 -0600

    added model folders and starter code

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS cookieconfig_db;
+
+CREATE DATABASE cookieconfig_db;
---
+++ models/index.js
@@ -0,0 +1,32 @@
+// ** THE ASSOCIATOINS WILL BE BETWEEN THE POST THAT BELONGS TO THE USER, THE COMMENTS ON THE POST THAT BELONG TO THAT USER AND THE POST BELONGS TO THE 
+
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
