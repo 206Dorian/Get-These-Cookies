3ee6fac5c763614f7516a91af6bca2e51a8c4217
Dorian Birch <206dorian@gmail.com>
2022-12-08 14:02:11 -0600

    changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,6 @@
-
-
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
 const postRoutes = require('./postRoutes');
-// const userRoutes = require('./userroutes');
-// const commentRoutes = require('./commentroutes');
-// const postRoutes = require('./postroutes');
 
 router.use('/user', userRoutes);
 router.use('/comment', commentRoutes);
